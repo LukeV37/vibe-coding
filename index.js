11e0e1de007056ff41325899f3371b02d7f64630
Clawd <luke@example.com>
2026-02-25 23:27:16 -0600

    Add ultimate vibe coding party app with puppy emojis and confetti

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  socket.emit('welcome', {
+    message: 'Welcome to the Ultimate Vibe Coding Party! 🎉',
+    funFact: 'Did you know puppies dream about you? 🐾',
+    joke: 'Why don\'t scientists trust atoms? Because they make up everything!'
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('Message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
